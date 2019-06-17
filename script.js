bf8c6e79c1bb9f14b2651ee0f202ad4b3992f353
cs-cognitome <gaya.scienza.v@gmail.com>
2019-06-17 21:44:53 +0300

    hover effect

---
+++ script.js
@@ -0,0 +1,9 @@
+
+$('.glitch').hover(function(){
+    var eLtext = $(this).text(),
+        eLchild = $(this).find('.glitch-link');
+    console.log(eLchild);
+    eLchild.attr('data-content', eLtext);
+    eLchild.toggleClass('blur');
+    $(this).toggleClass('active');
+  });
